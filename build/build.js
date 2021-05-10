7791196750a06cc369e5ebc404254bc7aa594de5
Léo Delplanque <delplanqueleomail@gmail.com>
2021-05-10 12:19:07 +0200

    first commit

---
+++ build/build.js
@@ -0,0 +1,115 @@
+var gui = new dat.GUI();
+var params = {
+    Download_Image: function () { return save(); },
+};
+gui.add(params, "Download_Image");
+var ai = new rw.HostedModel({
+    url: "https://earth-image-generator-48b3c158.hosted-models.runwayml.cloud/v1/",
+    token: "woX1y2sRQnFkGxG+qkwwoA==",
+});
+var img;
+var z = [];
+var a = [];
+var angle = 0;
+var frameNB = 0;
+function draw() {
+    if (img)
+        image(img, 0, 0, width, height);
+}
+function makeRequest() {
+    for (var i = 0; i < 512; i++) {
+        a[i] = z[i].value(angle);
+    }
+    var inputs = {
+        "z": a,
+        "truncation": 0.5,
+    };
+    ai.query(inputs).then(function (outputs) {
+        var image = outputs.image;
+        img = createImg(image);
+        img.hide();
+        p5.prototype.downloadFile(image, frameNB.toString(), "png");
+        frameNB++;
+        var da = TWO_PI / (24 * 60);
+        angle += da;
+        setTimeout(makeRequest, 200);
+    });
+}
+function setup() {
+    p6_CreateCanvas();
+    for (var i = 0; i < 512; i++) {
+        z[i] = new NoiseLoop(20, -1, 1);
+    }
+    makeRequest();
+}
+function windowResized() {
+    p6_ResizeCanvas();
+}
+var NoiseLoop = (function () {
+    function NoiseLoop(diameter, min, max) {
+        this.diameter = diameter;
+        this.min = min;
+        this.max = max;
+        this.cx = random(1000);
+        this.cy = random(1000);
+    }
+    NoiseLoop.prototype.value = function (a) {
+        var xoff = map(cos(a), -1, 1, this.cx, this.cx + this.diameter);
+        var yoff = map(sin(a), -1, 1, this.cy, this.cy + this.diameter);
+        var r = toxi.math.noise.simplexNoise.noise(xoff, yoff);
+        return map(r, -1, 1, this.min, this.max);
+    };
+    return NoiseLoop;
+}());
+var __ASPECT_RATIO = 1;
+var __MARGIN_SIZE = 25;
+function __desiredCanvasWidth() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return windowWidth - __MARGIN_SIZE * 2;
+    }
+    else {
+        return __desiredCanvasHeight() * __ASPECT_RATIO;
+    }
+}
+function __desiredCanvasHeight() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return __desiredCanvasWidth() / __ASPECT_RATIO;
+    }
+    else {
+        return windowHeight - __MARGIN_SIZE * 2;
+    }
+}
+var __canvas;
+function __centerCanvas() {
+    __canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+}
+function p6_CreateCanvas() {
+    __canvas = createCanvas(__desiredCanvasWidth(), __desiredCanvasHeight());
+    __centerCanvas();
+}
+function p6_ResizeCanvas() {
+    resizeCanvas(__desiredCanvasWidth(), __desiredCanvasHeight());
+    __centerCanvas();
+}
+var p6_SaveImageSequence = function (durationInFrames, fileExtension) {
+    if (frameCount <= durationInFrames) {
+        noLoop();
+        var filename_1 = nf(frameCount - 1, ceil(log(durationInFrames) / log(10)));
+        var mimeType = (function () {
+            switch (fileExtension) {
+                case 'png':
+                    return 'image/png';
+                case 'jpeg':
+                case 'jpg':
+                    return 'image/jpeg';
+            }
+        })();
+        __canvas.elt.toBlob(function (blob) {
+            p5.prototype.downloadFile(blob, filename_1, fileExtension);
+            setTimeout(function () { return loop(); }, 100);
+        }, mimeType);
+    }
+};
+//# sourceMappingURL=../src/src/build.js.map
---
+++ src/sketch.ts
@@ -0,0 +1,104 @@
+// -------------------
+//  Parameters and UI
+// -------------------
+
+const gui = new dat.GUI()
+const params = {
+    Download_Image: () => save(),
+}
+gui.add(params, "Download_Image")
+
+// You need to change this and use the values given to you by Runway for your own model
+const ai = new rw.HostedModel({
+    url: "https://earth-image-generator-48b3c158.hosted-models.runwayml.cloud/v1/",
+    token: "woX1y2sRQnFkGxG+qkwwoA==",
+});
+
+let img: p5.Element
+//// You can use the info() method to see what type of input object the model expects
+// model.info().then(info => console.log(info));
+
+let z = [];
+let a = [];
+let angle = 0;
+let frameNB = 0;
+// -------------------
+//       Drawing
+// -------------------
+
+function draw() {
+    if (img)
+        image(img, 0, 0, width, height)    
+}
+
+
+function makeRequest(){
+    
+    for (let i = 0; i < 512; i++) { //loop through all pixels, and select the corresponding value for the vector with the randomness generated from our Noise Loop function
+        a[i] = z[i].value(angle); 
+    }
+
+    
+
+    const inputs = {
+        "z": a,
+        "truncation": 0.5,
+    };
+
+    ai.query(inputs).then(outputs => {
+        const { image } = outputs;
+        img = createImg(image)
+        img.hide()
+        
+        p5.prototype.downloadFile(image, frameNB.toString(), "png");
+        frameNB++
+        let da = TWO_PI / (24*60); //MH - not sure why these values are used (1440 = 360*4)
+        angle += da;
+        //z = z.map(el=>el+random(-.2,.2));
+        //if (angle <= TWO_PI) { //once we have traversed all pixels, generated a new image
+            setTimeout(makeRequest, 200);
+          //}
+        
+    });
+}
+// -------------------
+//    Initialization
+// -------------------
+
+function setup() {
+    p6_CreateCanvas()    
+    for (let i = 0; i < 512; i++) {
+       z[i] = new NoiseLoop(20, -1, 1);
+    }
+    
+    makeRequest();
+}
+
+function windowResized() {
+    p6_ResizeCanvas()
+}
+
+class NoiseLoop {//introduces the randomness we need to generate images from the latent space
+    
+    diameter : number;
+    min : number;
+    max: number;
+    cy : number;
+    cx : number;
+
+    constructor(diameter, min, max) {
+      this.diameter = diameter;
+      this.min = min;
+      this.max = max;
+      this.cx = random(1000);
+      this.cy = random(1000);
+    }
+  
+    value(a) {
+      let xoff = map(cos(a), -1, 1, this.cx, this.cx + this.diameter);
+      let yoff = map(sin(a), -1, 1, this.cy, this.cy + this.diameter);
+      let r = toxi.math.noise.simplexNoise.noise(xoff,yoff); //requires toxic libs library
+      return map(r, -1, 1, this.min, this.max);
+    }
+  }
+
